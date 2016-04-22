9edc2d25f617be1d82e435634a5d4ff4840c85f5
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-21 19:08:54 -0700

    create object for business logic, connect to form on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+//business logic
+function Place(type, location, year, notes) {
+  this.type = type;
+  this.location = location;
+  this.year = year;
+  this.notes = notes;
+}
+
+//user interface logic
+$("#savePlace").submit(function(event) {
+  event.preventDefault();
+  var inputType = $("#placeType").val();
+  var inputLocation =$("#placeLocation").val();
+  var inputYear = $("#placeYear").val();
+  var inputNotes = $("#placeNotes").val();
+
+  var newPlace = new Place(inputType, inputLocation, inputYear, inputNotes);
+  var message = newPlace.year + ": <strong>" + newPlace.type + " in " + newPlace.location + "</strong> <br><em>" + newPlace.notes + "</em>";
+  $("#showPlaces").text(message);
+});
